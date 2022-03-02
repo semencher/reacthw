f418117a2d8954c8fdecf6532287532790e6b931
chernyshov.sv <sv.chernyshovgm@gmail.com>
2022-03-02 10:57:31 +0700

    2 hw 95%

--- hw2/index.js
+++ hw2/index.js
@@ -2,8 +2,8 @@
 const dynObj = (obj) => {
     return new Proxy(obj, {
         get(obj, prop) {
-            if (prop in obj) {
-                return obj[prop];
+            if (prop in obj || prop === "toJSON") {
+                    return obj[prop];
             } else {
                 obj[prop] = dynObj({});
                 return obj[prop];
@@ -16,6 +16,10 @@ const dynObj = (obj) => {
 
 const newObj = dynObj({a: 10});
 
-newObj.b.c.d;
+newObj.b.c.d = 25;
+newObj.b.c.l = 17;
+newObj.b.n.k = 98;
+newObj.b.n.f;
+
 
 console.log(JSON.stringify(newObj));
