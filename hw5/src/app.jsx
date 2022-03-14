bc80e04489813e48d1a13b68b628dfb741d756c5
chernyshov.sv <sv.chernyshovgm@gmail.com>
2022-03-14 15:53:16 +0700

    template for hw5

---
+++ hw5/src/app.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+
+const SimpleComponent = React.memo(({ number, componentRerenderedTimes }) => {
+  componentRerenderedTimes.current++;
+
+  const onPress = () => alert(number);
+
+  return <div onClick={() => onPress()}>Number: {number}</div>;
+});
+
+export default function App() {
+  const componentRerenderedTimes = React.useRef(0);
+  const divOfRenderTimes = React.createRef();
+  const [data, setData] = React.useState(
+    new Array(1000)
+      .fill({ number: 0 })
+      .map((item, index) => ({ number: item.number, id: String(index + 1) }))
+  );
+
+  const random = () =>
+    setData(
+      data.map(({ id }) => ({ number: Math.floor(1 + Math.random() * 10), id }))
+    );
+
+  useEffect(() => {
+    divOfRenderTimes.current.innerHTML = "Was rendered: " + componentRerenderedTimes.current;
+  });
+
+  return (
+    <div>
+      <div ref={divOfRenderTimes}></div>
+      <button onClick={() => random()}>random</button>
+      <button
+        onClick={() =>
+          setData(data => [{ number: 0, id: Math.random() }, ...data])
+        }
+      >
+        add to top
+      </button>
+      {data.map(item => (
+        <SimpleComponent
+          key={"sc-" + item.id}
+          number={item.number}
+          componentRerenderedTimes={componentRerenderedTimes}
+        />
+      ))}
+    </div>
+  );
+}
